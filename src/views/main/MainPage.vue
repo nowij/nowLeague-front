13359ef5a9f9ba25235f1fdb34be471205971a35
nowij <nowijdl@gmail.com>
2025-04-25 14:36:28 +0900

    ScheduleCard 컴포넌트 적용

--- src/views/main/MainPage.vue
+++ src/views/main/MainPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="container mx-auto px-4 py-8">
+  <main class="max-w-screen-xl mx-auto px-4 py-8">
     <!-- 리그 순위 -->
     <div class="mb-8 space-y-4">
       <div class="flex items-center gap-2">
@@ -15,7 +15,7 @@
           <TrophyIcon class="h-5 w-5 text-orange-500" />
           <h2 class="text-xl font-semibold">오늘의 경기 결과</h2>
         </div>
-        <ScheduleCard :date-value="String(20240315)"/>
+        <ScheduleCard v-if="todayResult" :schedule-value="todayResult" />
       </div>
 
       <!-- 다음 경기 -->
@@ -24,7 +24,7 @@
           <CalendarIcon class="h-5 w-5 text-blue-500" />
           <h2 class="text-xl font-semibold">다음 경기 일정</h2>
         </div>
-        <ScheduleCard :date-value="String(20240317)"/>
+        <ScheduleCard v-if="nextResult" :schedule-value="nextResult" />
       </div>
     </div>
   </main>
@@ -35,11 +35,16 @@ import { CalendarIcon, TrophyIcon, BarChart3Icon } from "lucide-vue-next"
 import { Ranking } from "@/views/result";
 import { ScheduleCard } from "@/views/shedule";
 import { onMounted, ref } from "vue";
+import {axiosWrapper} from "@/mixins";
 
 const todayMonth = ref('');
+const todayResult = ref(null);
+const nextResult = ref(null);
 
-onMounted(() => {
+onMounted(async () => {
   // getToday() // TODO
+  todayResult.value = await getResult(20240315);
+  nextResult.value = await getResult(20240316);
 })
 
 const getToday = () => {
@@ -53,6 +58,19 @@ const getToday = () => {
   todayMonth.value = year + month;
 
   return year + month + day
+}
+
+const getResult = async (dateValue) => {
+  const params = {
+    date: dateValue
+  }
+  try {
+    const response = await axiosWrapper.get('/game/schedule/date', {params})
+    return response.data
+  } catch (error) {
+    console.log('스케줄 조회 에러 ', error)
+    return {}
+  }
 }
 
 </script>
